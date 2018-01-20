e85cee06fcfa5fa357120b6d425e3bc5082d2e04
lvl5hm <level5hm@gmail.com>
2018-01-20 03:18:27 +0300

    added sprites

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import utils from './utils';
+import imgMarioRunning from './sprites/marioRunning.png';
+import imgMarioIdle from './sprites/marioIdle.png';
+import imgMarioJumping from './sprites/marioJumping.png';
 
 const canvas = document.querySelector('#game');
 const ctx = canvas.getContext('2d');
@@ -72,7 +75,7 @@ Object.values(keyCode).forEach(
   code => (keys[code] = { isDown: false, wentDown: false, wentUp: false })
 );
 
-document.onkeydown = function(event) {
+document.onkeydown = function (event) {
   const key = keys[event.keyCode];
   if (!key) {
     return;
@@ -83,7 +86,7 @@ document.onkeydown = function(event) {
   }
 };
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
   const key = keys[event.keyCode];
   if (!key) {
     return;
@@ -164,12 +167,12 @@ function createMap(asciiMapRows) {
   }
 }
 
-function checkCollision(entity, otherEntityType, offsetX = 0, offetY = 0) {
+function checkCollision(entity, otherEntityType, offsetX = 0, offsetY = 0) {
   const typeInfo = entityTypes[otherEntityType];
   for (let other of typeInfo.collisionList.items) {
     if (other !== utils.unorderedList.REMOVED_ITEM && entity !== other) {
       const eLeft = entity.x + offsetX + entity.bbox.left;
-      const eTop = entity.y + offetY + entity.bbox.top;
+      const eTop = entity.y + offsetY + entity.bbox.top;
       const eRight = eLeft + entity.bbox.width;
       const eBottom = eTop + entity.bbox.height;
 
@@ -246,6 +249,45 @@ function moveAndCheckForObstacles(entity, obstacleEntityType) {
   return isOnGround;
 }
 
+function loadSprite(fileName, offsetX = 0, offsetY = 0, frameCount = 1) {
+  const img = new Image();
+  img.src = fileName;
+  const sprite = {
+    bitmap: img,
+    width: 0,
+    height: 0,
+    frame: 0,
+    offsetX,
+    offsetY,
+    frameCount,
+  };
+  img.onload = () => {
+    sprite.width = img.width / frameCount;
+    sprite.height = img.height;
+  };
+
+  return sprite;
+}
+
+function drawSprite(sprite, x, y, speed = 0, scaleX = 1, scaleY = 1) {
+  const currentFrame = Math.floor(sprite.frame);
+  sprite.frame += speed;
+  if (sprite.frame >= sprite.frameCount) {
+    sprite.frame = 0;
+  }
+
+  ctx.save();
+  ctx.scale(scaleX, scaleY);
+  ctx.drawImage(
+    sprite.bitmap,
+    (currentFrame * sprite.width), 0, sprite.width, sprite.bitmap.height,
+    x * settings.pixelsPerMeter / scaleX + sprite.offsetX,
+    y * settings.pixelsPerMeter / scaleY + sprite.offsetY,
+    sprite.width, sprite.bitmap.height
+  );
+  ctx.restore();
+}
+
 // test code
 
 updateGame();
@@ -266,8 +308,12 @@ const ENTITY_TYPE_WALL = addEntityType('#', updateWall, {
     top: 0,
     width: settings.tileSize,
     height: settings.tileSize
-  }
+  },
 });
+
+const sprMarioRunning = loadSprite(imgMarioRunning, -76, -170, 2);
+const sprMarioJumping = loadSprite(imgMarioJumping, -76, -170);
+const sprMarioIdle = loadSprite(imgMarioIdle, -76, -170);
 
 function updateMario(mario) {
   const keyLeft = keys[keyCode.ARROW_LEFT];
@@ -282,10 +328,17 @@ function updateMario(mario) {
   const accelX = (keyRight.isDown - keyLeft.isDown) * metersPerSecondSq;
   const accelY = (keyDown.isDown - keyUp.isDown) * metersPerSecondSq;
 
+  if (keyRight.isDown) {
+    mario.direction = 1;
+  }
+  if (keyLeft.isDown) {
+    mario.direction = -1;
+  }
+
   mario.speedX += accelX * time.deltaTime;
-  mario.speedY += accelY * time.deltaTime; //settings.gravity * time.deltaTime;
+  mario.speedY += settings.gravity * time.deltaTime;
   mario.speedX *= 1 - friction * time.deltaTime;
-  mario.speedY *= 1 - friction * time.deltaTime;
+  //mario.speedY *= 1 - friction * time.deltaTime;
 
   const isOnGround = moveAndCheckForObstacles(mario, ENTITY_TYPE_WALL);
 
@@ -293,21 +346,32 @@ function updateMario(mario) {
     mario.speedY = -12;
   }
 
-  drawRect(
-    mario.x + mario.bbox.left,
-    mario.y + mario.bbox.top,
-    mario.bbox.width,
-    mario.bbox.height,
-    'yellow'
-  );
+  // drawRect(
+  //   mario.x + mario.bbox.left,
+  //   mario.y + mario.bbox.top,
+  //   mario.bbox.width,
+  //   mario.bbox.height,
+  //   'yellow'
+  // );
+  const absSpeedX = Math.abs(mario.speedX);
+  const dir = mario.direction || 1;
+
+  if (!isOnGround) {
+    // TODO сделать все спрайты правильного размера, чтобы их не нужно было масштабировать в юзеркоде
+    drawSprite(sprMarioJumping, mario.x, mario.y, 0, 0.4 * dir, 0.4);
+  } else if (absSpeedX > 1) {
+    drawSprite(sprMarioRunning, mario.x, mario.y, 0.03 * absSpeedX, 0.4 * dir, 0.4);
+  } else {
+    drawSprite(sprMarioIdle, mario.x, mario.y, 0, 0.4 * dir, 0.4);
+  }
 }
 
 const ENTITY_TYPE_MARIO = addEntityType('@', updateMario, {
   bbox: {
-    left: -0.2,
-    top: -0.6,
-    width: 0.4,
-    height: 0.6
+    left: -0.45,
+    top: -1,
+    width: 0.9,
+    height: 1
   }
 });
 
